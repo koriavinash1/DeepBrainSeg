02fcee6d7b21b16e7f1e28089f24be56ef6b9383
Avinash Kori <koriavinash1@gmail.com>
2022-05-20 14:47:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
      name='DeepBrainSeg',  
      version='0.2.1',
      author="Avinash Kori",
-     author_email="koriavinash1@gmail.com",
+     author_email="avin.kori.re@gmail.com",
      description="Deep Learning tool for brain tumor segmentation.",
      long_description=long_description,
      long_description_content_type="text/markdown",
